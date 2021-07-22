e19a5b808aaea993a7c6524219935e2db4dcb090
h-harsh <porwalharshp@gmail.com>
2021-07-22 15:53:16 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
 const app = express();
-const port = 8000
+const port = 7000
 
 app.get('/',(req, res) => {
     res.send("Hello Quiz")
 })
 
-app.listen(port => {
+app.listen(port, () => {
     console.log("app is listening, bolo")
 })
